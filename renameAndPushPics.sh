f052914cea863455e7c0222f13aa42040ef504ee
Xin Sun <accessun@hotmail.com>
2017-01-09 17:24:57 +0800

    added script for auto-renaming picture files

---
+++ renameAndPushPics.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+java -jar ./RenameAndMove-0.0.1-SNAPSHOT.jar
---
+++ source/RenameAndMove.java
@@ -0,0 +1,50 @@
+package io.github.accessun.main;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class RenameAndMove {
+    
+    static List<String> fileTypesToProcess = Arrays.asList("png", "jpg", "jpeg");
+
+    public void run() throws IOException {
+        Path dir = Paths.get(System.getProperty("user.dir"));
+
+        Files.list(dir).forEach(p -> {
+
+            String parentDir = p.getParent().toString();
+            String newName = UUID.randomUUID().toString();
+            String ext = FilenameUtils.getExtension(p.getFileName().toString()).toLowerCase();
+
+            if (Files.isRegularFile(p) && fileTypesToProcess.contains(ext)) {
+
+                String picDirStr = parentDir + File.separator + "pictures";
+                Path picDir = Paths.get(picDirStr);
+                Path newPath = Paths.get(picDirStr + File.separator + newName + "." + ext);
+
+                if (Files.exists(picDir) && Files.isDirectory(picDir)) {
+                    try {
+                        Files.move(p, newPath);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        });
+    }
+
+    public static void main(String[] args) throws IOException {
+        new RenameAndMove().run();
+    }
+
+}
